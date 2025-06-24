c702c6af67b2affa97bb7dd40bcdedf29ed1cad2
donghwan <kdhe9797@naver.com>
2025-06-24 11:35:50 +0900

    feat: 기초 세팅

---
+++ src/main/java/com/nsmm/esg/scope_service/ScopeServiceApplication.java
@@ -0,0 +1,16 @@
+package com.nsmm.esg.scope_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+
+public class ScopeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScopeServiceApplication.class, args);
+	}
+
+}
