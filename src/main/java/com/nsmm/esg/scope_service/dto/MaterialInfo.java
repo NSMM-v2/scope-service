c18d0bfdf0ee1f703841940d631fb366078db7af
donghwan <kdhe9797@naver.com>
2025-07-22 11:55:17 +0900

    feat: 자재코드 할당 기능 완료

--- src/main/java/com/nsmm/esg/scope_service/dto/MaterialInfo.java
+++ src/main/java/com/nsmm/esg/scope_service/dto/MaterialInfo.java
@@ -33,10 +33,6 @@ public class MaterialInfo {
     @Schema(description = "자재 카테고리", example = "금속")
     private String materialCategory;
 
-    @Size(max = 500, message = "자재 스펙은 500자 이하여야 합니다")
-    @Schema(description = "자재 스펙", example = "두께: 10mm, 크기: 1000x2000mm")
-    private String materialSpec;
-
     @Size(max = 1000, message = "자재 설명은 1000자 이하여야 합니다")
     @Schema(description = "자재 설명", example = "고강도 스틸 플레이트로 건설 및 제조업에 사용")
     private String materialDescription;
